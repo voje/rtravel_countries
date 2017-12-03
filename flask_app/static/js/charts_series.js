2709faf88beb6f7777e06d5b5477c2a19b25d8b6
voje <kristjan.voje@gmail.com>
2017-12-03 13:17:55 +0100

    todo: series bottleneck

--- flask_app/static/js/charts_series.js
+++ flask_app/static/js/charts_series.js
@@ -5,14 +5,18 @@ var charts = []
 // for debugging
 var tm
 
+console.time("main")
+
 // Main
 get_data(draw_chart)
 
 function get_data(_callback) {
+    console.time("get_data")
     d3.json("/data/submissions1", (d) => {
         data = d
         _callback()
     })
+    console.timeEnd("get_data")
 }
 
 reset_filters = function() {
@@ -22,9 +26,11 @@ reset_filters = function() {
 }
 
 render_all = function() {
+    console.time("render_all")
     charts.forEach((d)=> {
         d.render()
     })
+    console.timeEnd("render_all")
 }
 
 window.reset_all = function() {
@@ -33,45 +39,61 @@ window.reset_all = function() {
 }
 
 function count_all(data) {
+    console.time("count_all")
     var all = 0
     data.forEach( (d) => {
         all += d.value
     })
+    console.timeEnd("count_all")
     return all
 }
 
 function draw_chart() {
+    console.time("draw_chart")
     charts[0] = dc.seriesChart("#series-chart")
     cdata = crossfilter(data)
 
     // first, get an array of top 20 countries by using reduceCount
     // then filter out all other countries and remove them
     var filter_countries = []
-    console.log(cdata.size())
     var dim_filter_countries = cdata.dimension((d) => { return d.country })
+
+    console.time("dim_filter_countries1")
     dim_filter_countries.group().reduceCount().top(20).forEach((d) => {
         filter_countries.push(d.key)
     })
+    console.timeEnd("dim_filter_countries1")
+    console.time("dim_filter_countries2")
     dim_filter_countries.filterFunction((d) => {
         return filter_countries.indexOf(d) < 0  // filter all that are NOT in the list
     })
+    console.timeEnd("dim_filter_countries2")
     cdata.remove() // remove everything that matches the current filter
     dim_filter_countries.filterAll() //reset filter
-    console.log(cdata.size())
 
 
     // UTC to Date
+    console.time("data_utc_time")
     data.forEach((d)=>{
-        d.created_utc = new Date(d.created_utc * 1000)
+        // convert to Date format + round to month
+        d.created_utc = d3.time.month(new Date(d.created_utc * 1000))
     })
+    console.timeEnd("data_utc_time")
 
-    var dim_series = cdata.dimension((d) => { return [d.country, d3.time.month(d.created_utc)] })
+    console.time("dim_series")
+    //huge bottlenect (50s)
+    // TODO write your own reduce function to optimize the bottleneck
+    // graph date-value while mapping to countries in a dictionary cntry: [indices]
+    var dim_series = cdata.dimension((d) => { return [d.created_utc, d.country] })
+    console.timeEnd("dim_series")
     var grp_series = dim_series.group().reduceCount((d) => { return d.country })
+    tm = grp_series
 
+    console.time("minmax")
     var min_max = d3.extent(data, (d)=>{ return d.created_utc }) 
+    console.timeEnd("minmax")
 
     // series chart
-    tm = charts[0]
     charts[0]
         .colors(d3.scale.category20())
         .yAxisLabel("hits per month")
@@ -82,8 +104,8 @@ function draw_chart() {
         .brushOn(false)
         .dimension(dim_series)
         .group(grp_series)
-        .seriesAccessor(function(d) { return d.key[0];}) //d.key = [ cntry, date ]
-        .keyAccessor(function(d) {return d.key[1];})
+        .seriesAccessor(function(d) {return d.key[1];}) //d.key = [ date, cntry ]
+        .keyAccessor(function(d) {return d.key[0];})
         .valueAccessor(function(d) {return d.value;}) //d.value = count
         .legend(dc.legend()
             .x(50)
@@ -98,4 +120,7 @@ function draw_chart() {
 
 
     render_all()
+    console.timeEnd("draw_chart")
 }
+
+console.timeEnd("main")
