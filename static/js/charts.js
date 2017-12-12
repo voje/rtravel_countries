b85f927849d5aabc7c8c58efd11e52d732d679eb
voje <kristjan.voje@gmail.com>
2017-12-12 21:29:32 +0100

    gh-pages init

---
+++ static/js/charts.js
@@ -0,0 +1,122 @@
+var data = null
+var cdata = null
+var charts = []
+
+// Main
+get_data(draw_chart)
+
+function get_data(_callback) {
+    d3.json("data.json", (d) => {
+        data = d
+        _callback()
+    })
+}
+
+reset_filters = function() {
+    charts.forEach((d)=> {
+        d.filter(null)
+    })
+}
+
+render_all = function() {
+    charts.forEach((d)=> {
+        d.render()
+    })
+}
+
+window.reset_all = function() {
+    reset_filters()
+    render_all()
+}
+
+function count_all(data) {
+    var all = 0
+    data.forEach( (d) => {
+        all += d.value
+    })
+    return all
+}
+
+function draw_chart() {
+    charts[0] = dc.pieChart("#pie-chart")
+    charts[1] = dc.barChart("#bar-chart")
+    cdata = crossfilter(data)
+
+    // UTC to Date
+    data.forEach((d)=>{
+        d.created_utc = new Date(d.created_utc * 1000)
+    })
+
+    // In case we needed a filter, we would need a separate dimension just for filtering.
+    //dim_num = cdata.dimension((d) => { return d.count })
+    //dim_num.filter((d) => { return d > 0 })
+
+    // pie chart 
+    var dim_cntry = cdata.dimension((d) => { return d.country })
+    var grp_cntry = dim_cntry.group()
+    var count_cntry = grp_cntry.reduceCount()
+
+    // bar chart
+    var dim_time = cdata.dimension((d) => { return d.created_utc })
+    var grp_time = dim_time.group(d3.time.month)
+    var count_per_month = grp_time.reduceCount()
+
+
+    // pie chart
+    charts[0] 
+        //.width(1000)
+        .height(450)
+        .slicesCap(25)
+        .cx(435)        //x offset
+        //.innerRadius(100)
+        //.externalLabels(100)
+        .externalRadiusPadding(50)
+        //.drawPaths(false)
+        .dimension(dim_cntry)
+        .group(count_cntry)
+        .legend(dc.legend())
+    // example of formatting the legend via svg
+    // http://stackoverflow.com/questions/38430632/how-can-we-add-legends-value-beside-of-legend-with-proper-alignment
+    charts[0].on('pretransition', function(chart) {
+        var percent_helper = 0
+        var idx = 0
+        charts[0].selectAll('.dc-legend-item text')
+            .text('')
+            .append('tspan')
+            .text(function(d) { 
+                idx++
+                var sidx = "" + idx
+                var str = ""
+                if (sidx.length < 2) {
+                    str += " "
+                }
+                str += sidx + ". " + d.name
+                return str
+            })
+            .append('tspan')
+            .attr('x', 200)
+            .attr('text-anchor', 'end')
+            .text(function(d) {
+                percent_helper += d.data
+                return d.data
+            })
+          .append('tspan')
+            .attr('x', 220)
+            .attr('text-anchor', 'start')
+            .text(function(d) { 
+                return ( "(" + ( 100*d.data/percent_helper ).toFixed(1) + "%)" )
+            })
+    })
+
+    var min_max = d3.extent(data, (d)=>{ return d.created_utc }) 
+
+    // bar chart
+    charts[1]
+      .dimension(dim_time)
+      .group(count_per_month)
+      .x(d3.time.scale()
+        .domain([ min_max[0], min_max[1] ])
+        )
+
+    render_all()
+}
---
+++ static/js/charts_series.js
@@ -0,0 +1,124 @@
+var data = null
+var cdata = null
+var charts = []
+
+// for debugging
+var tm1
+var tm2
+var tm3
+
+
+// Main
+get_data(draw_chart)
+
+function get_data(_callback) {
+    d3.json("data.json", (d) => {
+        data = d
+        _callback()
+    })
+}
+
+reset_filters = function() {
+    charts.forEach((d)=> {
+        d.filter(null)
+    })
+}
+
+render_all = function() {
+    charts.forEach((d)=> {
+        d.render()
+    })
+}
+
+window.reset_all = function() {
+    reset_filters()
+    render_all()
+}
+
+function count_all(data) {
+    var all = 0
+    data.forEach( (d) => {
+        all += d.value
+    })
+    return all
+}
+
+function draw_chart() {
+    charts[0] = dc.seriesChart("#series-chart")
+    cdata = crossfilter(data)
+
+    // first, get an array of top 20 countries by using reduceCount
+    // then filter out all other countries and remove them
+    var filter_countries = []
+    var dim_filter_countries = cdata.dimension((d) => { return d.country })
+
+    // Remove "united states" data.
+    dim_filter_countries.filterFunction((d)=>{ return d == "united states" })
+    cdata.remove()
+    dim_filter_countries.filter(null) // remove this filter
+
+    dim_filter_countries.group().reduceCount().top(20).forEach((d) => {
+        filter_countries.push(d.key)
+    })
+
+
+    dim_filter_countries.filterFunction((d) => {
+        return filter_countries.indexOf(d) < 0  // filter all that are NOT in the list
+    })
+    cdata.remove() // remove everything that matches the current filter
+    dim_filter_countries.filterAll() //reset filter
+
+
+    // UTC to Date
+    data.forEach((d)=>{
+        // convert to Date format + round to month + convert back to int representation
+        // !!! using int instead of Date object is a huge performance boost for crossfilter !!!
+        d.created_utc = d3.time.month(new Date(d.created_utc * 1000)).getTime()
+    })
+
+    var dim_series = cdata.dimension((d) => {
+        //Each iteration is constant (cca 0.35ms). This will scale with data.  
+        //Iterations represent 1/3 of processing time. 2/3 is the time the function
+        //needs from last iteration to exit. 
+        return [d.created_utc, d.country] 
+    })
+    //var grp_series = dim_series.group().reduceCount((d) => { return d.country })
+    var grp_series = dim_series.group()
+
+    var min_max = d3.extent(data, (d)=>{ return d.created_utc }) 
+
+    // Sort legend alphabetically. 
+    dc.override(charts[0], 'legendables', function() {
+        var items = charts[0]._legendables();
+        return items.sort((a,b) => { return a < b });
+    });
+
+    // series chart
+    charts[0]
+        .title((d) => { return "[" + new Date(d.key[0]).toDateString() + ", " + d.key[1] + "]: " + d.value })
+        .colors(d3.scale.category20())
+        .yAxisLabel("hits per month")
+        .x(d3.time.scale()
+            .domain([ min_max[0], min_max[1] ])
+        )
+        .height(500)
+        .brushOn(false)
+        .dimension(dim_series)
+        .group(grp_series)
+        .seriesAccessor(function(d) {return d.key[1];}) //d.key = [ date, cntry ]
+        .keyAccessor(function(d) {return d.key[0];})
+        .valueAccessor(function(d) {return d.value;}) //d.value = count
+        .legend(dc.legend()
+            .x(50)
+            .y(charts[0].height() - 70)
+            .itemHeight(13)
+            .gap(5)
+            .legendWidth(1000) 
+            .horizontal(1)
+            .itemWidth(180)
+        )
+        .margins().bottom = 120
+
+
+    render_all()
+}
